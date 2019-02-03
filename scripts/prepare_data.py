78e8844fcd70b795c847db090c829a6814d407a7
Tautvydas Cerniauskas <tautvydas.cer@gmail.com>
2019-02-03 15:27:38 +0000

    Fixed all the issues with the data processing

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -12,12 +12,12 @@ tf.flags.DEFINE_integer(
 tf.flags.DEFINE_integer("max_sentence_len", 160, "Maximum sentence length")
 
 tf.flags.DEFINE_string(
-    "input_dir", os.path.abspath("../../data"),
-    "Input directory containing original CSV data files (default = '../../data')")
+    "input_dir", os.path.abspath("../data"),
+    "Input directory containing original CSV data files (default = '../data')")
 
 tf.flags.DEFINE_string(
-    "output_dir", os.path.abspath("../../data"),
-    "Output directory for TFrEcord files (default = '../../data')")
+    "output_dir", os.path.abspath("../data"),
+    "Output directory for TFrecord files (default = '../data')")
 
 FLAGS = tf.flags.FLAGS
 
@@ -47,7 +47,7 @@ def create_vocab(input_iter, min_frequency):
         FLAGS.max_sentence_len,
         min_frequency=min_frequency,
         tokenizer_fn=tokenizer_fn)
-    vocab_processor.fit(input_dir)
+    vocab_processor.fit(input_iter)
     return vocab_processor
 
 def transform_sentence(sequence, vocab_processor):
@@ -113,14 +113,17 @@ def create_example_test(row, vocab):
         dis_len_key = "distractor_{}".format(i)
         # Distractor Length feature
         dis_len = len(next(vocab._tokenizer([distractor])))
-        example.features.feature[dis_len_key].int64_list.value.extend(dis_transformed)
+        example.features.feature[dis_len_key].int64_list.value.extend([dis_len])
+        # Distractor Text Feature
+        dis_transformed = transform_sentence(distractor, vocab)
+        example.features.feature[dis_key].int64_list.value.extend(dis_transformed)
     return example
 
 def create_tfrecords_file(input_filename, output_filename, example_fn):
     """
     Creates a TFRecords file for the given input data and example transformation function
     """
-    writer = tf.python_io.TFRecordsWriter(output_filename)
+    writer = tf.python_io.TFRecordWriter(output_filename)
     print("Creating TFRecords file at {}...". format(output_filename))
     for i, row in enumerate(create_csv_iter(input_filename)):
         x = example_fn(row)
@@ -143,7 +146,7 @@ def write_vocabulary(vocab_processor, outfile):
 if __name__ == "__main__":
     print("Creating vocabulary...")
     input_iter = create_csv_iter(TRAIN_PATH)
-    input_iter = (x[o] + " " + x[1] for x in input_iter)
+    input_iter = (x[0] + " " + x[1] for x in input_iter)
     vocab = create_vocab(input_iter, min_frequency=FLAGS.min_word_frequency)
     print("Total vocabulary size: {}".format(len(vocab.vocabulary_)))
 
@@ -157,17 +160,16 @@ if __name__ == "__main__":
     create_tfrecords_file(
         input_filename=VALIDATION_PATH,
         output_filename=os.path.join(FLAGS.output_dir, "validation.tfrecords"),
-        example_fn(functools.partial(create_example_test, vocab=vocab))
+        example_fn=functools.partial(create_example_test, vocab=vocab))
 
-    # Create validation.tfrecords
+    # Create test.tfrecords
     create_tfrecords_file(
         input_filename=TEST_PATH,
         output_filename=os.path.join(FLAGS.output_dir, "test.tfrecords"),
-        example_fn(functools.partial(create_example_test, vocab=vocab))
+        example_fn=functools.partial(create_example_test, vocab=vocab))
 
-    # Create validation.tfrecords
+    # Create train.tfrecords
     create_tfrecords_file(
         input_filename=TRAIN_PATH,
         output_filename=os.path.join(FLAGS.output_dir, "train.tfrecords"),
-        example_fn(functools.partial(create_example_train, vocab=vocab))
-
+        example_fn=functools.partial(create_example_train, vocab=vocab))
