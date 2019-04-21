c4f54ba145b8de7782d03c36640dc5e24108beb9
Tautvydas Cerniauskas <tautvydas.cer@gmail.com>
2019-04-21 20:39:25 +0100

    Created a file to run training and evaluation

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-# python 
+eval="main.py --eval --name $2 -b $3"
+train="main.py --train -name $2"
+PYTHON=`which python3`
+
+if [[ $1 = "train" ]]; then
+    $PYTHON $train
+elif [[ $1 = "eval" ]]; then
+    $PYTHON $eval 
+else
+    echo "Please choose between train or eval"
+fi
+# $PYTHON $script_name
