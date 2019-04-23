18c3975c4fa5c3346b73072a2107ea6f9f576de4
Tautvydas Cerniauskas <tautvydas.cer@gmail.com>
2019-04-23 12:14:19 +0100

    Final version for presentation

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+inst="pip install -r requirements.txt"
 eval="main.py --eval --name $2 -b $3"
 train="main.py --train -name $2"
-PYTHON=`which python3`
+PYTHON=`which python`
 
 if [[ $1 = "train" ]]; then
     $PYTHON $train
 elif [[ $1 = "eval" ]]; then
     $PYTHON $eval 
+elif [[ $1 = "install" ]]
+    $inst
 else
     echo "Please choose between train or eval"
 fi
